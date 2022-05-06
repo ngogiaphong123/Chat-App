c3814757b4a27674eb41b4a043c0c71996af013f
Ngo Gia Phong <ngogiaphong123@gmail.com>
2022-05-06 19:34:37 +0700

    Logic when sned a message

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,12 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 const socket = io();
 //? Listen form server socket
 socket.on('message', message => {
     console.log(message);
     outputMessage(message);
+    //? Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 })
 
 //? Message submit
@@ -12,10 +15,17 @@ chatForm.addEventListener('submit', (event) => {
     const msg = event.target.elements.msg.value;
     //? Emit message to server
     socket.emit('chatMessage', msg);
+    //? Clear input
+    event.target.elements.msg.value = '';
+    event.target.elements.msg.focus();
 })
 
 const outputMessage = (message) => {
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHtml = ``;
+    div.innerHTML = `<p class="meta">Brad<span>9:12pm</span></p>
+    <p class="text">
+        ${message}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
 }
